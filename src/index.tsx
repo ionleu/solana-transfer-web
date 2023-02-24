9ae1b09188c8ad89fab2b2c8fcdd65b2d4111664
Ion Leu - Backbase <ion@backbase.com>
2023-02-23 21:07:31 -0500

    feature(search): instaled algolia dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -5,22 +5,27 @@ import * as buffer from "buffer";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AutoConnectProvider, WalletAdapterContext } from "./components";
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch } from "react-instantsearch-hooks-web";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 import "react-toastify/dist/ReactToastify.css";
 import "./assets/css/index.css";
 
 window.Buffer = buffer.Buffer;
+const searchClient = algoliasearch("YourApplicationID", "YourSearchOnlyAPIKey");
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <AutoConnectProvider>
-    <WalletAdapterContext>
-      <App />
-    </WalletAdapterContext>
-  </AutoConnectProvider>
+  <InstantSearch searchClient={searchClient} indexName="instant_search">
+    <AutoConnectProvider>
+      <WalletAdapterContext>
+        <App />
+      </WalletAdapterContext>
+    </AutoConnectProvider>
+  </InstantSearch>
 );
 
 // If you want to start measuring performance in your app, pass a function
